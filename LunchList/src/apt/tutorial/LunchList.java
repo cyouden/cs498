935efdcfc3c479be80b996d7569c6242cf6ac74b
Christopher Youden <cyouden@mines.edu>
2012-05-05 22:29:03 -0600

    adding tutorial skeleton

---
+++ LunchList/src/apt/tutorial/LunchList.java
@@ -0,0 +1,13 @@
+package apt.tutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LunchList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
