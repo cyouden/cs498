8307c0bee6051ef5b4b1f7b5ae136569ac508d5e
Christopher Youden <cyouden@mines.edu>
2012-05-06 14:22:11 -0600

    finished ch04 (added ListView for restaurants)

--- LunchList/src/apt/tutorial/LunchList.java
+++ LunchList/src/apt/tutorial/LunchList.java
@@ -1,14 +1,20 @@
 package apt.tutorial;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
-    Restaurant r = new Restaurant();
+    List<Restaurant> model = new ArrayList<Restaurant>();
+    ArrayAdapter<Restaurant> adapter = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,19 @@ public class LunchList extends Activity {
         Button save = (Button)findViewById(R.id.save);
         
         save.setOnClickListener(onSave);
+        
+        ListView list = (ListView)findViewById(R.id.restaurants);
+        
+        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
+        
+        list.setAdapter(adapter);
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
 		
 		public void onClick(View v) {
+			Restaurant r = new Restaurant();
+			
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.address);
 			RadioGroup types = (RadioGroup)findViewById(R.id.types);
@@ -39,6 +53,8 @@ public class LunchList extends Activity {
 				case R.id.delivery:
 					r.setType("sit_down");
 			}
+			
+			adapter.add(r);
 		}
 	};
 }
--- LunchList/src/apt/tutorial/Restaurant.java
+++ LunchList/src/apt/tutorial/Restaurant.java
@@ -28,4 +28,8 @@ public class Restaurant {
 	public void setType(String type) {
 		this.type = type;
 	}
+	
+	public String toString() {
+		return getName();
+	}
 }
