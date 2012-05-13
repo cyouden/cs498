5f4913138e91ce9b62e6818fa41a77dfe259fd90
Christopher Youden <cyouden@mines.edu>
2012-05-13 17:18:40 -0600

    finished ch05 (made a better list layout)

--- LunchList/src/apt/tutorial/LunchList.java
+++ LunchList/src/apt/tutorial/LunchList.java
@@ -5,16 +5,74 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class LunchList extends Activity {
+	public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+		RestaurantAdapter() {
+			super(LunchList.this, R.layout.row, model);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			RestaurantHolder holder = null;
+			
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				
+				row = inflater.inflate(R.layout.row, parent, false);
+				holder = new RestaurantHolder(row);
+				row.setTag(holder);
+			}
+			else {
+				holder = (RestaurantHolder)row.getTag();
+			}
+			
+			holder.populateFrom(model.get(position));
+			
+			return row;
+		}
+	}
+		
+	static class RestaurantHolder {
+		private TextView name = null;
+		private TextView address = null;
+		private ImageView icon = null;
+		
+		RestaurantHolder(View row) {
+			name = (TextView)row.findViewById(R.id.title);
+			address = (TextView)row.findViewById(R.id.address_row_view);
+			icon = (ImageView)row.findViewById(R.id.icon);
+		}
+		
+		
+		void populateFrom(Restaurant r) {
+			name.setText(r.getName());
+			address.setText(r.getAddress());
+			
+			if (r.getType().equals("sit_down")) {
+				icon.setImageResource(R.drawable.ball_red);
+			}
+			else if (r.getType().equals("take_out")) {
+				icon.setImageResource(R.drawable.ball_yellow);
+			}
+			else {
+				icon.setImageResource(R.drawable.ball_green);
+			}
+		}
+	}
+	
     List<Restaurant> model = new ArrayList<Restaurant>();
-    ArrayAdapter<Restaurant> adapter = null;
+    RestaurantAdapter adapter = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +85,7 @@ public class LunchList extends Activity {
         
         ListView list = (ListView)findViewById(R.id.restaurants);
         
-        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
+        adapter = new RestaurantAdapter();
         
         list.setAdapter(adapter);
     }
@@ -48,10 +106,13 @@ public class LunchList extends Activity {
 			switch (types.getCheckedRadioButtonId()) {
 				case R.id.sit_down:
 					r.setType("sit_down");
+					break;
 				case R.id.take_out:
-					r.setType("sit_down");
+					r.setType("take_out");
+					break;
 				case R.id.delivery:
-					r.setType("sit_down");
+					r.setType("delivery");
+					break;
 			}
 			
 			adapter.add(r);
