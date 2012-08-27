456c2bce5d3c3ee8aacf6b5f8720a1a9ce85d219
Christopher Youden <cyouden@mines.edu>
2012-08-26 20:15:59 -0600

    Changed UI to a button that displays the current time

---
+++ HelloAndroid/src/cyouden/csci498/helloandroid/Now.java
@@ -0,0 +1,29 @@
+package cyouden.csci498.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
+
+public class Now extends Activity implements View.OnClickListener {
+	Button btn;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		
+		btn = new Button(this);
+		btn.setOnClickListener(this);
+		updateTime();
+		setContentView(btn);
+	}
+	
+	public void onClick(View view) {
+		updateTime();
+	}
+	
+	private void updateTime() {
+		btn.setText(new Date().toString());
+	}
+}
