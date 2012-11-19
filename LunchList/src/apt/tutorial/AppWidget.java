40f5dc3009caaa068f7b6f49fb681eec374835a9
Christopher Youden <cyouden@mines.edu>
2012-11-18 21:14:26 -0700

    Finished Ch 22 (Added random restaurant widget)

---
+++ LunchList/src/apt/tutorial/AppWidget.java
@@ -0,0 +1,13 @@
+package apt.tutorial;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class AppWidget extends AppWidgetProvider {
+	@Override
+	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] addWidgetIds) {
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
+}
